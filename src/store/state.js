688e6c7b2196c829b655cff88b562efa76eae47d
jakovchuk <jakovchukas@gmail.com>
2022-04-20 00:47:59 +0300

    update state.js

--- src/store/state.js
+++ src/store/state.js
@@ -1,23 +1,14 @@
-var works = [{ id: 0,
-                date: '',
-                projName: '',
-                workType: '',
-                time: ''}]
-var tdate = ''
-var tprojName = ''
-var tworkType = ''
-var ttime = ''
-var butType = 0
-var curNum = 0
-var worksChange = 0
-
 export default {
-  works,
-  tdate,
-  tprojName,
-  tworkType,
-  ttime,
-  butType,
-  curNum,
-  worksChange
+  works: [{ id: 0,
+            date: '',
+            projName: '',
+            workType: '',
+            time: ''}],
+  tdate: '',
+  tprojName: '',
+  tworkType: '',
+  ttime: '',
+  butType: 0,
+  curNum: 0,
+  worksChange: 0,
 }
